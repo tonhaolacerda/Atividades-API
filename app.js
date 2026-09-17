1d08ab0d69acd406f7f89b0fa594b5c41975cf3c
tonhaolacerda <anthony.souto@estudante.ifms.edu.br>
2026-09-17 17:42:10 +0000

    Atividade número 2

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express();
 
 const port = 3000;
 
+const tarefas = [
+    { id: 1, titulo: "Fazer atividades de PTAC", concluida: false },
+    { id: 2, titulo: "Estudar banco de dados", concluida: true },
+    { id: 3, titulo: "Desligar servidor", concluida: false }
+];
+
+app.get("/tarefas", (res,req) => {
+    res.json(tarefas);
+});
+
 
 app.get('/', (req,res, next) => {
     console.log('API de tarefas no ar')
